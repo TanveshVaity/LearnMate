22cf17877de0bf7c4e45ed6f3de3d7c7d8f147ad
TanveshVaity <tanveshvaity99@gmail.com>
2025-07-17 11:19:35 +0530

    added dark theme mode

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,18 @@
+import { ModeToggle } from "./ModeToggle";
 import { Button } from "./ui/button";
 
 
 export default function Navbar(){
     return (
-        <div className="mt-4 mb-8">
-            <div className="flex justify-between items-center px-10">
+        <div className="mb-8">
+            <div className="flex justify-between items-center px-10 py-4 border-b-2 dark:border-b-[#2c3035]">
                 <div>
                     <p className="font-bold">
                         LearnMate
                     </p>
                 </div>
-                <div>
+                <div className="flex items-center gap-6">
+                    <ModeToggle />
                     <Button>Get Started</Button>
                 </div>
             </div>
